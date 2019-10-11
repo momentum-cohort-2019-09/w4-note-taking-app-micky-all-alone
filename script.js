3c1a1b0d2c834efebc07218c1e37b3ef334e582f
mickymck <mickymckeon@gmail.com>
2019-10-11 15:02:03 -0400

    need to fix my duplicate notes

--- script.js
+++ script.js
@@ -10,71 +10,113 @@ function basicAuthCreds() {
   return 'Basic ' + btoa('mickyMcK:mickypassword')
 }
 
-function createNewNote() {
-  document.querySelector(".submit-button").addEventListener("click", function (event) {
-    event.preventDefault()
-    let title = document.querySelector('.input-title').value
-    let text = document.querySelector('.input-text').value
-    return fetch('https://notes-api.glitch.me/api/notes', {
-      method: 'POST',
-      body: JSON.stringify({ "title": title, "text": text }),
+function runNotesApp() {
+
+  let notesArray = []
+
+  retrieveNotes()
+
+  function retrieveNotes() {
+    fetch('https://notes-api.glitch.me/api/notes', {
+      method: 'GET',
       headers: {
-        'Content-Type': 'application/json',
         'Authorization': basicAuthCreds()
       }
     })
       .then(response => response.json())
-      .then(JSONresponse => notesArray.push(JSONresponse))
-  })
-}
 
-createNewNote()
+      .then(function (JSONresponse) {
 
-let notesArray = []
+        notesArray = JSONresponse.notes.slice(0)
+        renderNotes()
+      })
+  }
 
-function retrieveNotes() {
-  fetch('https://notes-api.glitch.me/api/notes', {
-    method: 'GET',
-    headers: {
-      'Authorization': basicAuthCreds()
+  function renderNotes() {
+
+    for (let note of notesArray) {
+
+      const title = note.title
+      const text = note.text
+      const id = note._id
+      const noteDiv = document.querySelector("#note-body")
+
+      noteDiv.innerHTML = ''
+
+      noteDiv.innerHTML +=
+        `
+              <div class="note-container" id="${id}">
+                <div class="title">
+                  <h1 class="note-title">
+                    ${title}
+                  </h1>
+                </div>
+                <div class="text">
+                  <p>
+                    ${text}
+                  </p>
+                </div>
+                <button type="edit" class="edit-button">Edit</button>
+                <button type="delete" class="delete-button">Delete</button>
+              </div>
+            `
     }
-  })
-    .then(response => response.json())
+  }
 
-    .then(function (JSONresponse) {
+  function renderLatestNote() {
+    for (let note of notesArray) {
 
-      let notesArray = JSONresponse.notes.slice(0)
-      console.log(notesArray)
+      const title = note.title
+      const text = note.text
+      const id = note._id
+      const noteDiv = document.querySelector("#note-body")
 
-      for (let note of notesArray) {
+      noteDiv.innerHTML +=
+        `
+              <div class="note-container" id="${id}">
+                <div class="title">
+                  <h1 class="note-title">
+                    ${title}
+                  </h1>
+                </div>
+                <div class="text">
+                  <p>
+                    ${text}
+                  </p>
+                </div>
+                <button type="edit" class="edit-button">Edit</button>
+                <button type="delete" class="delete-button">Delete</button>
+              </div>
+            `
+    }
+  }
 
-        const title = note.title
-        const text = note.text
-        const id = note._id
-        const noteDiv = document.querySelector("#note-body")
-
-        noteDiv.innerHTML +=
-          `
-  <div class="note-container" id="${id}">
-    <div class="title">
-      <h1 class="note-title">
-        ${title}
-      </h1>
-    </div>
-    <div class="text">
-      <p>
-        ${text}
-      </p>
-    </div>
-    <button type="edit" class="edit-button">Edit</button>
-    <button type="delete" class="delete-button">Delete</button>
-  </div>
-`
-      }
+  function createNewNote() {
+    document.querySelector(".submit-button").addEventListener("click", function (event) {
+      event.preventDefault()
+      let title = document.querySelector('.input-title').value
+      let text = document.querySelector('.input-text').value
+      return fetch('https://notes-api.glitch.me/api/notes', {
+        method: 'POST',
+        body: JSON.stringify({ "title": title, "text": text }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': basicAuthCreds()
+        }
+      })
+        .then(response => response.json())
+        .then(function (JSONresponse) {
+          notesArray.push(JSONresponse)
+          renderNotes()
+        })
     })
+  }
+
+  createNewNote()
 }
 
-retrieveNotes()
+  runNotesApp()
+
 
 // function deleteNote () {
 //   console.log("delete note")
